71a53fe4ed5bd9d448e27ec62055dc99bd7799cf
thien.vuong <thien.vuong@nois.vn>
2023-05-01 05:26:41 +0700

    Step simple of Stair

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 
+export interface ISize {
+  width?: number;
+  height?: number;
+  depth?: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = '3DTrainingRoom';
+
+  scale = 1/10;
+
+  room: ISize = {
+    width: 3500 * this.scale,
+    depth: 7300 * this.scale
+  }
+
+  step_count = 15;
+  step: ISize = {
+    height: 170 * this.scale,
+    depth: 280 * this.scale
+  }
+
+  step_big_count = Math.ceil(this.step_count / 2);
+  step_big: ISize = {
+    height: 170 * 2 * this.scale,
+    depth: 280 * 2 * this.scale
+  }
 }
